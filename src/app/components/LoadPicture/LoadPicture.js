695ba12733926be48b1161d7188b82bad6ed9c30
MinhDong <dongdang@success-ss.com.vn>
2018-07-13 18:06:32 +0700

    Issue with height and width at small screens

--- src/app/components/LoadPicture/LoadPicture.js
+++ src/app/components/LoadPicture/LoadPicture.js
@@ -4,7 +4,7 @@ import './LoadPicture.scss';
 const LoadPicture = () => (
   <div className="load">
     <div className="img-load text-center">
-      <img className="image-gs" src="https://media0.giphy.com/media/xTk9ZvMnbIiIew7IpW/giphy-downsized.gif" alt="" />
+      <img className="img-fluid image-gs" src="https://media0.giphy.com/media/xTk9ZvMnbIiIew7IpW/giphy-downsized.gif" alt="" />
     </div>
   </div>
 );
--- src/app/containers/Home/Home.js
+++ src/app/containers/Home/Home.js
@@ -18,9 +18,6 @@ class Home extends Component {
   }
 
   componentDidMount() {
-    $(document.body).scroll(() => {
-      console.log($(this).scrollTop());
-    });
     const effect = new TimelineMax({ paused: true });
     effect.from('.name', 0.5, { scale: 2, opacity: 0 })
       .to('.name', 0.5, { scale: 1, opacity: 1 })
@@ -28,105 +25,143 @@ class Home extends Component {
       .from('.header-fixed', 1, { y: -50, opacity: 0.5, ease: Bounce.easeOut })
       .from('.slider', 1, { opacity: 0, y: 100, ease: Bounce.easeOut })
       .from('.hr', 0.5, { x: -1000, opacity: 0 });
-
-    const gallery1 = new TimelineMax({ paused: true });
-    gallery1.staggerFrom('img.img-fluid.r1-c1', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery2 = new TimelineMax({ paused: true });
-    gallery2.staggerFrom('img.img-fluid.r1-c2', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery3 = new TimelineMax({ paused: true });
-    gallery3.staggerFrom('img.img-fluid.r1-c3', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery4 = new TimelineMax({ paused: true });
-    gallery4.staggerFrom('img.img-fluid.r2-c1', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery5 = new TimelineMax({ paused: true });
-    gallery5.staggerFrom('img.img-fluid.r2-c2', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery6 = new TimelineMax({ paused: true });
-    gallery6.staggerFrom('img.img-fluid.r2-c3', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery7 = new TimelineMax({ paused: true });
-    gallery7.staggerFrom('img.img-fluid.r3-c1', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery8 = new TimelineMax({ paused: true });
-    gallery8.staggerFrom('img.img-fluid.r3-c2', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery9 = new TimelineMax({ paused: true });
-    gallery9.staggerFrom('img.img-fluid.r3-c3', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery10 = new TimelineMax({ paused: true });
-    gallery10.staggerFrom('img.img-fluid.r4-c1', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery11 = new TimelineMax({ paused: true });
-    gallery11.staggerFrom('img.img-fluid.r4-c2', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
-    const gallery12 = new TimelineMax({ paused: true });
-    gallery12.staggerFrom('img.img-fluid.r4-c3', 1, {
-      opacity: 0, y: 300,
-    }, 0.5);
     function Gallery() {
-      const offset1 = $('img.img-fluid.r1-c1').offset();
-      console.log(offset1);
-      const offset2 = $('img.img-fluid.r1-c2').offset();
-      const offset3 = $('img.img-fluid.r1-c3').offset();
-      const offset4 = $('img.img-fluid.r2-c1').offset();
-      const offset5 = $('img.img-fluid.r2-c2').offset();
-      const offset6 = $('img.img-fluid.r2-c3').offset();
-      const offset7 = $('img.img-fluid.r3-c1').offset();
-      const offset8 = $('img.img-fluid.r3-c2').offset();
-      const offset9 = $('img.img-fluid.r3-c3').offset();
-      const offset10 = $('img.img-fluid.r4-c1').offset();
-      const offset11 = $('img.img-fluid.r4-c2').offset();
-      const offset12 = $('img.img-fluid.r4-c3').offset();
-      $(window).scroll(() => {
-        if ($(window).scrollTop() >= (offset1.top - 500)) {
+      const gallery1 = new TimelineMax({ paused: true });
+      gallery1.staggerFrom('img.img-fluid.r1-c1', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery2 = new TimelineMax({ paused: true });
+      gallery2.staggerFrom('img.img-fluid.r1-c2', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery3 = new TimelineMax({ paused: true });
+      gallery3.staggerFrom('img.img-fluid.r1-c3', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery4 = new TimelineMax({ paused: true });
+      gallery4.staggerFrom('img.img-fluid.r2-c1', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery5 = new TimelineMax({ paused: true });
+      gallery5.staggerFrom('img.img-fluid.r2-c2', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery6 = new TimelineMax({ paused: true });
+      gallery6.staggerFrom('img.img-fluid.r2-c3', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery7 = new TimelineMax({ paused: true });
+      gallery7.staggerFrom('img.img-fluid.r3-c1', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery8 = new TimelineMax({ paused: true });
+      gallery8.staggerFrom('img.img-fluid.r3-c2', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery9 = new TimelineMax({ paused: true });
+      gallery9.staggerFrom('img.img-fluid.r3-c3', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery10 = new TimelineMax({ paused: true });
+      gallery10.staggerFrom('img.img-fluid.r4-c1', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery11 = new TimelineMax({ paused: true });
+      gallery11.staggerFrom('img.img-fluid.r4-c2', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const gallery12 = new TimelineMax({ paused: true });
+      gallery12.staggerFrom('img.img-fluid.r4-c3', 1, {
+        opacity: 0, y: 300,
+      }, 1);
+      const offset1 = $('img.img-fluid.r1-c1').offset().top;
+      const offset2 = $('img.img-fluid.r1-c2').offset().top;
+      const offset3 = $('img.img-fluid.r1-c3').offset().top;
+      const offset4 = $('img.img-fluid.r2-c1').offset().top;
+      const offset5 = $('img.img-fluid.r2-c2').offset().top;
+      const offset6 = $('img.img-fluid.r2-c3').offset().top;
+      const offset7 = $('img.img-fluid.r3-c1').offset().top;
+      const offset8 = $('img.img-fluid.r3-c2').offset().top;
+      const offset9 = $('img.img-fluid.r3-c3').offset().top;
+      const offset10 = $('img.img-fluid.r4-c1').offset().top;
+      const offset11 = $('img.img-fluid.r4-c2').offset().top;
+      const offset12 = $('img.img-fluid.r4-c3').offset().top;
+      $(window).on('scroll', () => {
+        if ($(window).scrollTop() >= (offset1 - window.innerHeight)) {
           gallery1.play();
-          if ($(window).scrollTop() >= (offset2.top - 500)) {
-            gallery2.play();
-          }
-          if ($(window).scrollTop() >= (offset3.top - 500)) {
-            gallery3.play();
-          }
-          if ($(window).scrollTop() >= (offset4.top - 500)) {
-            gallery4.play();
-          }
-          if ($(window).scrollTop() >= (offset5.top - 500)) {
-            gallery5.play();
-          }
-          if ($(window).scrollTop() >= (offset6.top - 500)) {
-            gallery6.play();
-          }
-          if ($(window).scrollTop() >= (offset7.top - 500)) {
-            gallery7.play();
-          }
-          if ($(window).scrollTop() >= (offset8.top - 500)) {
-            gallery8.play();
-          }
-          if ($(window).scrollTop() >= (offset9.top - 500)) {
-            gallery9.play();
-          }
-          if ($(window).scrollTop() >= (offset10.top - 500)) {
-            gallery10.play();
-          }
-          if ($(window).scrollTop() >= (offset11.top - 500)) {
-            gallery11.play();
-          }
-          if ($(window).scrollTop() >= (offset12.top - 500)) {
-            gallery12.play();
-          }
+        }
+        if ($(window).scrollTop() >= (offset2 - window.innerHeight)) {
+          gallery2.play();
+        }
+        if ($(window).scrollTop() >= (offset3 - window.innerHeight)) {
+          gallery3.play();
+        }
+        if ($(window).scrollTop() >= (offset4 - window.innerHeight)) {
+          gallery4.play();
+        }
+        if ($(window).scrollTop() >= (offset5 - window.innerHeight)) {
+          gallery5.play();
+        }
+        if ($(window).scrollTop() >= (offset6 - window.innerHeight)) {
+          gallery6.play();
+        }
+        if ($(window).scrollTop() >= (offset7 - window.innerHeight)) {
+          gallery7.play();
+        }
+        if ($(window).scrollTop() >= (offset8 - window.innerHeight)) {
+          gallery8.play();
+        }
+        if ($(window).scrollTop() >= (offset9 - window.innerHeight)) {
+          gallery9.play();
+        }
+        if ($(window).scrollTop() >= (offset10 - window.innerHeight)) {
+          gallery10.play();
+        }
+        if ($(window).scrollTop() >= (offset11 - window.innerHeight)) {
+          gallery11.play();
+        }
+        if ($(window).scrollTop() >= (offset12 - window.innerHeight)) {
+          gallery12.play();
+        }
+      });
+      $(window).on('touchmove', (e) => {
+        const touch = e.touches[0];
+        console.log(Math.floor(touch.pageY));
+        if (touch.pageY >= (offset1)) {
+          gallery1.play();
+        }
+        if (touch.pageY > (offset2 + 32)) {
+          gallery2.play();
+        }
+        if (touch.pageY >= (offset3 + 250)) {
+          gallery3.play();
+        }
+        if (touch.pageY >= (offset4) + 400) {
+          gallery4.play();
+        }
+        if (touch.pageY >= (offset5) + 600) {
+          gallery5.play();
+        }
+        if (touch.pageY >= (offset6 + 800)) {
+          gallery6.play();
+        }
+        if (touch.pageY >= offset7 + 1000) {
+          gallery7.play();
+        }
+        if (touch.pageY >= (offset8 + 1000)) {
+          gallery8.play();
+        }
+        if (touch.pageY >= offset9 + 1000) {
+          gallery9.play();
+        }
+        if (touch.pageY >= offset10 + 1000) {
+          gallery10.play();
+        }
+        if (touch.pageY >= offset11 + 1000) {
+          gallery11.play();
+        }
+        if (touch.pageY >= offset12 + 1000) {
+          gallery12.play();
         }
       });
     }
@@ -134,19 +169,18 @@ class Home extends Component {
       effect.play();
     }
     const showGallery = new TimelineMax({ onComplete: Gallery });
-    console.log(showGallery);
-    // showGallery.staggerFrom('img.img-fluid.r1-c1', 1, { opacity: 0 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r1-c2', 1, { opacity: 0, y: 300 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r1-c3', 1, { opacity: 0, y: 300 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r2-c1', 1, { opacity: 0, y: 300 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r2-c2', 1, { opacity: 0, y: 300 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r2-c3', 1, { opacity: 0, y: 300 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r3-c1', 1, { opacity: 0, y: 300 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r3-c2', 1, { opacity: 0, y: 300 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r3-c3', 1, { opacity: 0, y: 300 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r4-c1', 1, { opacity: 0, y: 300 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r4-c2', 1, { opacity: 0, y: 300 }, 0.5)
-    //   .staggerFrom('img.img-fluid.r4-c3', 1, { opacity: 0, y: 300 }, 0.5);
+    showGallery.staggerFrom('img.img-fluid.r1-c1', 0.01, { opacity: 0 })
+      .staggerFrom('img.img-fluid.r1-c2', 0.001, { opacity: 0, y: 300 }, 0.5)
+      .staggerFrom('img.img-fluid.r1-c3', 0.001, { opacity: 0, y: 300 }, 0.5)
+      .staggerFrom('img.img-fluid.r2-c1', 0.001, { opacity: 0, y: 300 }, 0.5)
+      .staggerFrom('img.img-fluid.r2-c2', 0.001, { opacity: 0, y: 300 }, 0.5)
+      .staggerFrom('img.img-fluid.r2-c3', 0.001, { opacity: 0, y: 300 }, 0.5)
+      .staggerFrom('img.img-fluid.r3-c1', 0.001, { opacity: 0, y: 300 }, 0.5)
+      .staggerFrom('img.img-fluid.r3-c2', 0.001, { opacity: 0, y: 300 }, 0.5)
+      .staggerFrom('img.img-fluid.r3-c3', 0.001, { opacity: 0, y: 300 }, 0.5)
+      .staggerFrom('img.img-fluid.r4-c1', 0.001, { opacity: 0, y: 300 }, 0.5)
+      .staggerFrom('img.img-fluid.r4-c2', 0.001, { opacity: 0, y: 300 }, 0.5)
+      .staggerFrom('img.img-fluid.r4-c3', 0.001, { opacity: 0, y: 300 }, 0.5);
     const showContentWeb = new TimelineMax({ onComplete: show });
     showContentWeb.from('.img-load.text-center', 1.5, { opacity: 0 })
       .to('.img-load.text-center', 1, { opacity: 1 })
